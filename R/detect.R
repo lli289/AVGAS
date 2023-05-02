7a3a07e1d6f068bf34542420590b9748d8458bf6
Lillian Li <lli289.git@gmail.com>
2023-05-01 20:06:45 -0400

    Create detect.R

---
+++ R/detect.R
@@ -0,0 +1,127 @@
+#' Suggesting values for \code{r2}
+#'
+#' This function suggests the values for \code{r2}.
+#'
+#' @param X Input data. An optional data frame, or numeric matrix of dimension
+#' \code{n} by \code{nmain.p}. Note that the two-way interaction effects should not
+#' be included in \code{X} because this function automatically generates the
+#' corresponding two-way interaction effects if needed.
+#' @param y Response variable. A \code{n}-dimensional vector, where \code{n} is the number
+#' of observations in \code{X}.
+#' @param heredity Whether to enforce Strong, Weak, or No heredity. Default is "Strong".
+#' @param nmain.p A numeric value that represents the total number of main effects
+#' in \code{X}.
+#' @param sigma The standard deviation of the noise term. In practice, sigma is usually
+#' unknown. In such case, this function automatically estimate sigma using root mean
+#' square error (RMSE). Default is NULL. Otherwise, users need to enter a numeric value.
+#' @param r1 A numeric value indicating the maximum number of main effects. This number
+#' can be different from the \code{r1} defined in \code{\link{inter_detection}}.
+#' @param r2 A numeric value indicating the maximum number of interaction effects.
+#' This number can be different from the \code{r2} defined in \code{\link{inter_detection}}.
+#' @param interaction.ind A two-column numeric matrix containing all possible
+#' two-way interaction effects. It must be generated outside of this function
+#' using \code{t(utils::combn())}. See Example section for details.
+#' @param pi1 A numeric value between 0 and 1, defined by users. Default is 0.32.
+#' For guidance on selecting an appropriate value, please refer to \code{\link{ABC}}.
+#' @param pi2 A numeric value between 0 and 1, defined by users. Default is 0.32.
+#' For guidance on selecting an appropriate value, please refer to \code{\link{ABC}}.
+#' @param pi3 A numeric value between 0 and 1, defined by users. Default is 0.32.
+#' For guidance on selecting an appropriate value, please refer to \code{\link{ABC}}.
+#' @param lambda A numeric value defined by users. Default is 10.
+#' For guidance on selecting an appropriate value, please refer to \code{\link{ABC}}.
+#' @param q A numeric value indicating the number of models in each generation (e.g.,
+#' the population size). Default is 40.
+#'
+#' @return A \code{list} of output. The components are:
+#' \item{InterRank}{Rank of all candidate interaction effects. A two-column numeric
+#' matrix. The first column contains indices of ranked two-way interaction effects, and the
+#' second column contains its corresponding ABC score.}
+#' \item{mainind.sel}{Selected main effects.  A \code{r1}-dimensional vector.}
+#' \item{mainpool}{Ranked main effects in \code{X}.}
+#' \item{plot}{Plot of potential interaction effects and their corresponding ABC scores.}
+#' @export
+#'
+#' @seealso \code{\link{initial}}.
+#'
+#' @examples # under Strong heredity
+#' set.seed(0)
+#' nmain.p <- 4
+#' interaction.ind <- t(combn(4,2))
+#' X <- matrix(rnorm(50*4,1,0.1), 50, 4)
+#' epl <- rnorm(50,0,0.01)
+#' y<- 1+X[,1]+X[,2]+X[,1]*X[,2]+epl
+#' d1 <- detect(X, y, nmain.p = 4, r1 = 3, r2 = 3,
+#'     interaction.ind = interaction.ind, q = 5)
+#' @examples # under No heredity
+#' d2 <- detect(X, y, heredity = "No", nmain.p = 4, r1 = 3, r2 = 3,
+#'     interaction.ind = interaction.ind, q = 5)
+#'
+#'
+detect <- function (X, y, heredity = "Strong",
+                    nmain.p, sigma = NULL,  r1, r2,
+                    interaction.ind = NULL,
+                    pi1 = 0.32, pi2 = 0.32, pi3 = 0.32,
+                    lambda = 10, q = 40){
+  if (is.null(interaction.ind)) stop("Interaction.ind is missing.
+                                       Use t(utils::combn()) to generate interaction matrix.")
+  colnames(X) <- make.names(rep("","X",ncol(X)+1),unique=TRUE)[-1]
+  max_model_size <- r1 + r2
+
+  DCINFO <- VariableScreening::screenIID(X, y, method="DC-SIS")
+  mainind <- as.numeric(gsub(".*?([0-9]+).*", "\\1",  colnames(X)[order(DCINFO$rank)]))
+  Shattempind <- mainind[1:r1]
+
+  # no heredity pool
+  if (heredity == "No"){
+    interpooltemp <- interaction.ind
+  }
+
+  # weak heredity pool
+  if (heredity =="Weak"){
+    for (i in 1:q) {
+      df <- rbind(interaction.ind[interaction.ind[,1] %in% Shattempind,][order(stats::na.exclude(match(interaction.ind[,1], Shattempind))),],
+                  interaction.ind[interaction.ind[,2] %in% Shattempind,][order(stats::na.exclude(match(interaction.ind[,2], Shattempind))),])
+      interpooltemp <- df[!duplicated(df),]
+    }
+  }
+
+  # strong heredity pool
+  if (heredity =="Strong"){
+    interpooltemp <- t(utils::combn(sort(Shattempind),2))
+  }
+
+  intercandidates.ind <- match(do.call(paste, as.data.frame(interpooltemp)), do.call(paste, as.data.frame(interaction.ind)))+nmain.p
+
+  interscoreind <- list()
+  for (i in 1:length(intercandidates.ind)) {
+    interscoreind[[i]] <- ABC(X, y, heredity = heredity, nmain.p = nmain.p, sigma = sigma,
+                              extract = "Yes", varind = c(Shattempind,intercandidates.ind[i]),
+                              interaction.ind = interaction.ind,
+                              pi1 = pi1, pi2 = pi2, pi3= pi3, lambda = lambda)
+  }
+  interscoreind <- interscoreind
+  MA <- as.matrix(cbind(inter = intercandidates.ind, scores = interscoreind))
+  if (dim(MA)[1] == 1){
+    MB <- MA
+  }else{
+    MB <- MA[order(unlist(MA[,2]), na.last = TRUE),]
+  }
+  interpool <- MB
+  if (dim(interpool)[1] <= 50){
+    gp <- ggplot2::ggplot(as.data.frame(interpool),
+                          ggplot2::aes(x = reorder(as.character(inter),
+                                                   +as.numeric(scores)), y = as.numeric(scores))) +
+      geom_point() + theme(axis.text.x = element_text(angle = 90)) +
+      labs(y = "ABC Scores", x = "Interaction Effects")
+  }else{
+    gp <- ggplot2::ggplot(as.data.frame(interpool)[1:50,],
+                          ggplot2::aes(x = reorder(as.character(inter),
+                                                   +as.numeric(scores)), y = as.numeric(scores))) +
+      geom_point() + theme(axis.text.x = element_text(angle = 90)) +
+      labs(y = "ABC Scores", x = "Interaction Effects")
+  }
+  return(list(InterRank = interpool,
+              mainind.sel = Shattempind,
+              mainpool = mainind,
+              plot = gp))
+}
