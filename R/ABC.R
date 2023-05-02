2ec0e8a3fa2b4ff9dd4fc8050f9622fe7c7975ef
Lillian Li <lli289.git@gmail.com>
2023-05-01 20:06:47 -0400

    Update ABC.R

--- R/ABC.R
+++ R/ABC.R
@@ -24,6 +24,9 @@
 #' @param heredity Whether to enforce Strong, Weak, or No heredity. Default is "Strong".
 #' @param nmain.p A numeric value that represents the total number of main effects
 #' in \code{X}.
+#' @param sigma The standard deviation of the noise term. In practice, sigma is usually
+#' unknown. In such case, this function automatically estimate sigma using root mean
+#' square error (RMSE). Default is NULL. Otherwise, users need to enter a numeric value.
 #' @param extract A either "Yes" or "No" logical vector that represents whether or not
 #' to extract specific columns from \code{X}. Default is "No".
 #' @param varind Only used when \code{extract = "Yes"}. A numeric vector of class
@@ -51,7 +54,7 @@
 #' @references
 #' Ye, C. and Yang, Y., 2019. \emph{High-dimensional adaptive minimax sparse estimation with interactions.}
 #'
-#' @examples
+#' @examples # sigma is unknown
 #' set.seed(0)
 #' nmain.p <- 4
 #' interaction.ind <- t(combn(4,2))
@@ -61,10 +64,16 @@
 #' ABC(X, y, nmain.p = 4)
 #' ABC(X, y, nmain.p = 4, extract = "Yes",
 #'     varind = c(1,2,5), interaction.ind = interaction.ind)
+#'
+#' @examples # users want to enter a suggested value for sigma
+#' ABC(X, y, nmain.p = 4, sigma = 0.01)
+#' ABC(X, y, nmain.p = 4, sigma = 0.01, extract = "Yes",
+#'     varind = c(1,2,5), interaction.ind = interaction.ind)
+#'
 
-ABC <- function(X, y, heredity = "Strong", nmain.p, extract = "No", varind = NULL,
-                interaction.ind = NULL, pi1 = 0.32, pi2 = 0.32, pi3 = 0.32,
-                lambda = 10){
+ABC <- function(X, y, heredity = "Strong", nmain.p, sigma = NULL,
+                extract = "No", varind = NULL, interaction.ind = NULL,
+                pi1 = 0.32, pi2 = 0.32, pi3 = 0.32, lambda = 10){
   colnames(X) <- make.names(rep("","X",ncol(X)+1),unique=TRUE)[-1]
   n <- dim(X)[1]
   if (extract == "Yes"){
@@ -89,7 +98,12 @@ ABC <- function(X, y, heredity = "Strong", nmain.p, extract = "No", varind = NUL
     yhat <- (data)%*%solve(crossprod(data), t(data)%*%y, tol = 1e-50)
   }
   SSE <- sum((y - yhat)^2)
-  sigma = sqrt(SSE/(length((y - yhat))- length(allpar)))
+
+  if (is.null(sigma)){
+    sigma = sqrt(SSE/(length((y - yhat))- length(allpar)))
+  }else{
+    sigma = sigma
+  }
 
   if (heredity == "Strong"){
     C.I.strong <- -log(pi1)+log(min(nmain.p,n))+log(min(mychoose(k1),n))
