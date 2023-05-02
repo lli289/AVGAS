07f8a5e63e05899b25448cd95b6030cdb197f591
Lillian Li <lli289.git@gmail.com>
2023-05-02 01:21:33 -0400

    Update detect.R

--- R/detect.R
+++ R/detect.R
@@ -14,10 +14,8 @@
 #' @param sigma The standard deviation of the noise term. In practice, sigma is usually
 #' unknown. In such case, this function automatically estimate sigma using root mean
 #' square error (RMSE). Default is NULL. Otherwise, users need to enter a numeric value.
-#' @param r1 A numeric value indicating the maximum number of main effects. This number
-#' can be different from the \code{r1} defined in \code{\link{inter_detection}}.
+#' @param r1 A numeric value indicating the maximum number of main effects.
 #' @param r2 A numeric value indicating the maximum number of interaction effects.
-#' This number can be different from the \code{r2} defined in \code{\link{inter_detection}}.
 #' @param interaction.ind A two-column numeric matrix containing all possible
 #' two-way interaction effects. It must be generated outside of this function
 #' using \code{t(utils::combn())}. See Example section for details.
@@ -52,16 +50,49 @@
 #' y<- 1+X[,1]+X[,2]+X[,1]*X[,2]+epl
 #' d1 <- detect(X, y, nmain.p = 4, r1 = 3, r2 = 3,
 #'     interaction.ind = interaction.ind, q = 5)
+#'
 #' @examples # under No heredity
 #' d2 <- detect(X, y, heredity = "No", nmain.p = 4, r1 = 3, r2 = 3,
 #'     interaction.ind = interaction.ind, q = 5)
 #'
-#'
+
 detect <- function (X, y, heredity = "Strong",
                     nmain.p, sigma = NULL,  r1, r2,
                     interaction.ind = NULL,
                     pi1 = 0.32, pi2 = 0.32, pi3 = 0.32,
                     lambda = 10, q = 40){
+
+  bbb <- int(X, y, heredity = heredity,
+             nmain.p = nmain.p, sigma = sigma,  r1 = r1, r2 = r2,
+             interaction.ind = interaction.ind,
+             pi1 = pi1, pi2 = pi2, pi3 = pi3,
+             lambda = lambda, q = q)
+
+  interpool <- bbb$InterRank
+  ccc <- as.data.frame(interpool)
+  inter <- ccc[,1]
+  scores <- ccc[,2]
+  if (dim(interpool)[1] <= 50){
+    gp <- ggplot2::ggplot(ccc,
+                          ggplot2::aes(x = stats::reorder(as.character(inter),
+                                                   +as.numeric(scores)), y = as.numeric(scores))) +
+      geom_point() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) +
+      ggplot2::labs(y = "ABC Scores", x = "Interaction Effects")
+  }else{
+    gp <- ggplot2::ggplot(as.data.frame(interpool)[1:50,],
+                          ggplot2::aes(x = stats::reorder(as.character(inter),
+                                                   +as.numeric(scores)), y = as.numeric(scores))) +
+      geom_point() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) +
+      ggplot2::labs(y = "ABC Scores", x = "Interaction Effects")
+  }
+  return(plot = gp)
+}
+
+int <- function(X, y, heredity = "Strong",
+                 nmain.p, sigma = NULL,  r1, r2,
+                 interaction.ind = NULL,
+                 pi1 = 0.32, pi2 = 0.32, pi3 = 0.32,
+                 lambda = 10, q = 40){
   if (is.null(interaction.ind)) stop("Interaction.ind is missing.
                                        Use t(utils::combn()) to generate interaction matrix.")
   colnames(X) <- make.names(rep("","X",ncol(X)+1),unique=TRUE)[-1]
@@ -107,21 +138,8 @@ detect <- function (X, y, heredity = "Strong",
     MB <- MA[order(unlist(MA[,2]), na.last = TRUE),]
   }
   interpool <- MB
-  if (dim(interpool)[1] <= 50){
-    gp <- ggplot2::ggplot(as.data.frame(interpool),
-                          ggplot2::aes(x = reorder(as.character(inter),
-                                                   +as.numeric(scores)), y = as.numeric(scores))) +
-      geom_point() + theme(axis.text.x = element_text(angle = 90)) +
-      labs(y = "ABC Scores", x = "Interaction Effects")
-  }else{
-    gp <- ggplot2::ggplot(as.data.frame(interpool)[1:50,],
-                          ggplot2::aes(x = reorder(as.character(inter),
-                                                   +as.numeric(scores)), y = as.numeric(scores))) +
-      geom_point() + theme(axis.text.x = element_text(angle = 90)) +
-      labs(y = "ABC Scores", x = "Interaction Effects")
-  }
   return(list(InterRank = interpool,
               mainind.sel = Shattempind,
-              mainpool = mainind,
-              plot = gp))
+              mainpool = mainind))
 }
+
